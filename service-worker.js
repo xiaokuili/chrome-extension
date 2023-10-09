e141ed6c870a30d78e544917ce1182cc8accacbb
yinlin1991 <lenglengzailuguan@gamil.com>
2023-10-09 18:25:30 +0800

    修改标题和描述

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,13 @@
 // 监听来自content.js的消息
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  // const urlbase = "http://localhost:3000";
+  const urlbase = "https://use-ai-production.up.railway.app";
+  const data = {
+    prompt: request.data,
+  };
   if (request.action === "translate") {
-    const url = "http://localhost:3000/api/translate";
-    const data = {
-      prompt: request.data,
-    };
+    const url = urlbase + "/api/translate";
+
     fetch(url, {
       method: "POST", // *GET, POST, PUT, DELETE, etc.
       mode: "cors", // no-cors, *cors, same-origin
@@ -17,6 +20,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       redirect: "follow", // manual, *follow, error
       referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
       body: JSON.stringify(data), // body data type must match "Content-Type" header
+      timeout: 5000,
     })
       .then((response) => {
         if (!response.ok) {
@@ -33,10 +37,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   }
 
   if (request.action === "noun") {
-    const url = "http://localhost:3000/api/noun";
-    const data = {
-      prompt: request.data,
-    };
+    const url = urlbase + "/api/noun";
     fetch(url, {
       method: "POST", // *GET, POST, PUT, DELETE, etc.
       mode: "cors", // no-cors, *cors, same-origin
@@ -49,6 +50,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       redirect: "follow", // manual, *follow, error
       referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
       body: JSON.stringify(data), // body data type must match "Content-Type" header
+      timeout: 5000,
     })
       .then((response) => {
         if (!response.ok) {
