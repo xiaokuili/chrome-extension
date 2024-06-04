38d9af9ddaddcd8a9c5f4ed154c276996d26cbee
yin1991 <84140478+xiaokuili@users.noreply.github.com>
2024-06-04 12:04:58 +0000

    feat: Add functionality to close Zhihu question modals automatically
    This commit adds a new function `closeZhihuQuestion()` to the `content.ts` file. This function is responsible for closing the Zhihu question modals by clicking on the close button. It also includes a loop that runs every 3 milliseconds to check if the modal is present and close it if found. The loop stops after 10 iterations or if the modal is successfully closed.
    
    This feature improves the user experience by automatically closing the modals, allowing users to focus on the content without any interruptions.

--- fuck-zhihu-login/content.ts
+++ fuck-zhihu-login/content.ts
@@ -1,12 +1,26 @@
+function closeZhihuQuestion() {
+  const buttons = document.getElementsByClassName("Modal-closeButton")
+
+  if (buttons.length !== 0) {
+    buttons[0].click()
+    return true
+  }
+  return false
+}
+
 ;(function () {
   "use strict"
 
   if (location.href.indexOf("zhihu.com/question") != -1) {
-    const buttons = document.getElementsByClassName("Modal-closeButton")
+    let counter = 0
+    const intervalId = setInterval(() => {
+      const ok = closeZhihuQuestion()
+      counter++
 
-    if (buttons.length !== 0) {
-      buttons[0].click()
-    }
+      if (counter === 10 || ok) {
+        clearInterval(intervalId) // Stop the interval after 10 iterations
+      }
+    }, 3)
   }
 
   if (location.href.indexOf("zhihu.com/signin") != -1) {
